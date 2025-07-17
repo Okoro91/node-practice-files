4a4e0bf499f177356c94533e8b1a2acf5ca0f2a8
okoro91 <mi.fracri91@gmail.com>
2025-07-17 08:30:42 +0100

    added query and CORS

--- server/modules/sendJSON.js
+++ server/modules/sendJSON.js
@@ -1,5 +1,7 @@
 export const sendJSON = (res, statusCode, dataload) => {
   res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
   res.statusCode = statusCode;
   res.end(JSON.stringify(dataload));
 };
--- server/server.js
+++ server/server.js
@@ -2,15 +2,20 @@ import http from "node:http";
 import { getDataFromDB } from "./database/db.js";
 import { sendJSON } from "./modules/sendJSON.js";
 import { dataPath } from "./modules/dataPath.js";
+import { dataQuery } from "./modules/dataQuery.js";
 
 const PORT = 8000;
 
 const server = http.createServer(async (req, res) => {
   const getData = await getDataFromDB();
 
-  if (req.url === "/api" && req.method === "GET") {
-    const destinations = JSON.stringify(getData);
-    sendJSON(res, 200, destinations);
+  const urlObj = new URL(req.url, `http://${req.headers.host}`);
+
+  const queryObj = Object.fromEntries(urlObj.searchParams);
+
+  if (urlObj.pathname === "/api" && req.method === "GET") {
+    let filteredData = dataQuery(getData, queryObj);
+    sendJSON(res, 200, filteredData);
   } else if (req.url.startsWith("/api/continent") && req.method === "GET") {
     const continent = req.url.split("/").pop();
     const filteredData = dataPath(getData, "continent", continent);
