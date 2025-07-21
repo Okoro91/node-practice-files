a37d53a7ffd72128edbf43cbca4721586f3e3b51
okoro91 <mi.fracri91@gmail.com>
2025-07-21 09:50:16 +0100

    handled post request for new sighting

--- routing/Handlers/routeHandlers.js
+++ routing/Handlers/routeHandlers.js
@@ -1,5 +1,7 @@
 import { getData } from "../utils/getData.js";
 import { sendRes } from "../utils/sendRes.js";
+import { parseJSON } from "../utils/parseJSON.js";
+import { addNewSighting } from "../utils/addNewSighting.js";
 
 export const handleGet = async (res) => {
   const data = await getData();
@@ -8,5 +10,11 @@ export const handleGet = async (res) => {
 };
 
 export const handlePost = async (req, res) => {
-  console.log("post in");
+  try {
+    const parsedBody = await parseJSON(req);
+    await addNewSighting(parsedBody);
+    sendRes(res, 201, "application/json", JSON.stringify(parsedBody));
+  } catch (err) {
+    sendResponse(res, 400, "application/json", JSON.stringify({ error: err }));
+  }
 };
---
+++ routing/utils/addNewSighting.js
@@ -0,0 +1,12 @@
+import path from "node:path";
+import fs from "node:fs/promises";
+import { getData } from "./getData.js";
+
+export const addNewSighting = async (newSighting) => {
+  const sightings = await getData();
+  sightings.push(newSighting);
+
+  const pathJSON = path.join("data", "data.json");
+
+  await fs.writeFile(pathJSON, JSON.stringify(sightings, null, 2), "utf8");
+};
